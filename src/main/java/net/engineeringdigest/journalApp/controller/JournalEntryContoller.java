c1416950ee32eeef7651f201c8e7902d507fde45
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-11-13 23:54:57 +0545

    starting

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryContoller.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryContoller.java
@@ -1,6 +1,24 @@
 package net.engineeringdigest.journalApp.controller;
 
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
 public class JournalEntryContoller {
 
 
+    private Map<Long, JournalEntry> journalentity = new HashMap<>();
+
+    @GetMapping("/getAll")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalentity.values());
+    }
 }
